50a9e7b322b227e9dc19acc81704907525249410
Neeraj <contact@neerajx0.xyz>
2024-05-30 11:47:56 +0530

    fix

--- Face_rec.py
+++ Face_rec.py
@@ -1,111 +1,83 @@
 import face_recognition
-from tkinter import messagebox
+from tkinter import messagebox, Tk
 import cv2
 import os
 from datetime import datetime
 from database import save_face_data
 
-video_capture = 0
-
 class FaceScan:
-    def __init__(self, master):
-        self.master = master
+    def __init__(self, master=None):
+        self.master = master or Tk()
         self.master.title("Face Scanner")
-        ScanFace()
+        self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.video_capture = cv2.VideoCapture(0)
+        self.known_face_encodings, self.known_face_names, self.idname = self.load_known_faces("images")
+        self.process_this_frame = True
+        self.scan_face()
+        self.master.mainloop()
+
+    def on_closing(self):
+        self.video_capture.release()
+        cv2.destroyAllWindows()
         self.master.destroy()
-idname = {}         
-# Load images from the "images" folder and generate face encodings
-def load_known_faces(image_folder):
-    known_face_encodings = []
-    known_face_names = []
 
-    for filename in os.listdir(image_folder):
-        if filename.endswith(('.jpg', '.jpeg', '.png', '.gif')):
-            name = os.path.splitext(filename)[0]
-            image_path = os.path.join(image_folder, filename)
-            image = face_recognition.load_image_file(image_path)
-            # Get face encodings only if a face is detected
-            face_encodings = face_recognition.face_encodings(image)
-            if face_encodings:
-                known_face_encodings.append(face_encodings[0])
-                known_face_names.append(name.split("_")[0])
-                idname[name.split("_")[0]] = name.split("_")[1]
+    def load_known_faces(self, image_folder):
+        known_face_encodings = []
+        known_face_names = []
+        idname = {}
 
-    return known_face_encodings, known_face_names
+        for filename in os.listdir(image_folder):
+            if filename.endswith(('.jpg', '.jpeg', '.png')):
+                name = os.path.splitext(filename)[0]
+                image_path = os.path.join(image_folder, filename)
+                image = face_recognition.load_image_file(image_path)
+                face_encodings = face_recognition.face_encodings(image)
+                if face_encodings:
+                    known_face_encodings.append(face_encodings[0])
+                    known_face_names.append(name.split("_")[0])
+                    idname[name.split("_")[0]] = name.split("_")[1]
 
+        return known_face_encodings, known_face_names, idname
 
-# Function to save detected face data to a JSON file
+    def scan_face(self):
+        while True:
+            ret, frame = self.video_capture.read()
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
+            if self.process_this_frame:
+                face_locations = face_recognition.face_locations(small_frame)
+                face_encodings = face_recognition.face_encodings(small_frame, face_locations)
+                face_names = []
 
-# Function to scan for faces using webcam
-def ScanFace():
-    video_capture = cv2.VideoCapture(0)
-    # Initialize video capture from webcam
-    
+                for face_encoding in face_encodings:
+                    matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+                    name = "Unknown"
 
-    # Load known faces and their encodings
-    image_folder = "images"
-    known_face_encodings, known_face_names = load_known_faces(image_folder)
+                    if True in matches:
+                        first_match_index = matches.index(True)
+                        name = self.known_face_names[first_match_index]
+                        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        face_data = {"name": name, "time": timestamp, "student_id": self.idname[name]}
+                        if save_face_data(face_data):
+                            messagebox.showinfo("Face Recognition", f"Attendance marked for {name}")
 
-    # Initialize variables
-    process_this_frame = True
+                    face_names.append(name)
 
-    while True:
-        # Capture frame-by-frame
-        ret, frame = video_capture.read()
+            self.process_this_frame = not self.process_this_frame
 
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            for (top, right, bottom, left), name in zip(face_locations, face_names):
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom + 30), font, 1.0, (0, 255, 0), 1)
 
-        if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(small_frame)
-            face_encodings = face_recognition.face_encodings(small_frame, face_locations)
+            cv2.imshow('Video', frame)
 
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-                if True in matches:
-                    first_match_index = matches.index(True)
-                    name = known_face_names[first_match_index]
-
-                    # Save detected face data to a JSON file
-                    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    face_data = {"name": name, "time": timestamp,"student_id":idname[name]}
-                    isaved = save_face_data(face_data)
-                    if isaved:
-                        messagebox.showinfo("Face Recognition", f"Attendance marked for {name}")   
-                face_names.append(name)
-
-        process_this_frame = not process_this_frame
-
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-
-            # Draw a label with a name below the face
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom + 30), font, 1.0, (0, 255, 0), 1)
-
-        # Display the resulting frame
-        cv2.imshow('Video', frame)
-
-        # Hit 'q' on the keyboard to quit!
-        if cv2.waitKey(1) & 0xFF == ord('q') or ord('Q'):
-            break
-
-    # Release handle to the webcam
-    video_capture.release()
-    cv2.destroyAllWindows()
-
-
+if __name__ == "__main__":
+    FaceScan()
